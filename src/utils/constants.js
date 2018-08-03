001c76b56079a2d402ae200084cd6bc77cd80004
IdoJS <IdoJS@users.noreply.github.com>
2018-08-03 20:31:17 +0300

    utils
    - add constants
    - add value convert table

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export default {
+  HAND_TYPE: {
+    STRAIGHT_FLUSH: "straightFlush",
+    FOUR: "four",
+    FULL_HOUSE: "fullHouse",
+    FLUSH: "flush",
+    STRAIGHT: "straight",
+    THREE: "three",
+    TWO_PAIR: "twoPair",
+    ONE_PAIR: "onePair",
+    HIGH_CARD: "highCard"
+  }
+};
---
+++ src/utils/valuesConvertTable.js
@@ -0,0 +1,18 @@
+const valueMapping = {
+  A: 14,
+  K: 13,
+  Q: 12,
+  J: 11,
+  T: 10,
+  14: "A",
+  13: "K",
+  12: "Q",
+  11: "J",
+  10: "T"
+};
+
+const getValue = value => {
+  return valueMapping[value] || value;
+};
+
+export { getValue };
