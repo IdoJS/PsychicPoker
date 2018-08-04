640bb29a4a97853c58474334b3d998023d98e06a
IdoJS <IdoJS@users.noreply.github.com>
2018-08-04 17:39:24 +0300

    rule straight

---
+++ src/__tests__/logic/rules/straight.test.js
@@ -0,0 +1,50 @@
+import { isStraight } from "../../../logic/rules/straight";
+
+describe("Check isStraight", () => {
+  let hand = [];
+  let replace = [];
+  let result;
+
+  it("Hand: 6C 6D 8C 2D 6H Deck: ", () => {
+    hand = ["2C", "3D", "4H", "5D", "6S"];
+    replace = [];
+    result = isStraight(hand, replace);
+    expect(result.rank).toBe(5);
+    expect(JSON.stringify(result.cards)).toBe('["2C","3D","4H","5D","6S"]');
+  });
+
+  it("Hand: 6C 6D 8C 2D 6H Deck: ", () => {
+    hand = ["2C", "3D", "4H", "5D", "6S"];
+    replace = ["4H"];
+    result = isStraight(hand, replace);
+    expect(result.rank).toBe(15);
+  });
+
+  it("Hand: 6C 6D 8C 2D 6H Deck: ", () => {
+    hand = ["2C", "3D", "4H", "5D", "6S"];
+    replace = ["4H", "7C"];
+    result = isStraight(hand, replace);
+    expect(result.rank).toBe(15);
+  });
+
+  it("Hand: 6C 6D 8C 2D 6H Deck: ", () => {
+    hand = ["2C", "3D", "4H", "5D", "6S"];
+    replace = ["4H", "7C", "8D"];
+    result = isStraight(hand, replace);
+    expect(result.rank).toBe(15);
+  });
+
+  it("Hand: 6C 6D 8C 2D 6H Deck: ", () => {
+    hand = ["2C", "3D", "4H", "5D", "6S"];
+    replace = ["4H", "7C", "8D", "6S"];
+    result = isStraight(hand, replace);
+    expect(result.rank).toBe(15);
+  });
+
+  it("Hand: 6C 6D 8C 2D 6H Deck: ", () => {
+    hand = ["2C", "3D", "4H", "5D", "6S"];
+    replace = ["4H", "7C", "8D", "6S", "5C"];
+    result = isStraight(hand, replace);
+    expect(result.rank).toBe(5);
+  });
+});
---
+++ src/logic/rules/straight.js
@@ -0,0 +1,126 @@
+import baseResult from "../../utils/baseResult";
+import { getValue } from "../../utils/valuesConvertTable";
+import { combineGenerator } from "../../utils/combinations";
+/**
+ * input - userCards :5 cards
+ *        replaceFromDeck: [1..5] cards
+ * output - is high-card & hightest card value & card list
+ */
+
+const isStraight = (userCards = [], replaceFromDeck = []) => {
+  let replaceFromDeckLength = replaceFromDeck.length;
+  let straight = {
+    value: 0,
+    position1: -1,
+    position2: -1,
+    position3: -1,
+    position4: -1,
+    position5: -1,
+    cards: [],
+    rank: 15,
+    highest: 0
+  };
+
+  if (replaceFromDeck.length === 0) {
+    straight = searchOnList(straight, userCards);
+  } else if (replaceFromDeck.length === 5) {
+    straight = searchOnList(straight, replaceFromDeck);
+  } else {
+    straight = combinationsSearch(
+      straight,
+      userCards,
+      replaceFromDeck,
+      replaceFromDeckLength
+    );
+  }
+  return Object.assign({}, baseResult, straight);
+};
+
+const searchForStraight = list => {
+  let straight = {
+    value: 0,
+    position1: -1,
+    position2: -1,
+    position3: -1,
+    position4: -1,
+    position5: -1,
+    cards: [],
+    rank: 15,
+    highest: 0
+  };
+
+  let valueList = [];
+  for (let i = 0; i < list.length; i++) {
+    valueList.push(getValue(list[i]));
+  }
+
+  let sortedList = valueList.sort((a, b) => a - b);
+  let isStraightFlag = true;
+  for (let i = 0; i < sortedList.length - 1; i++) {
+    if (sortedList[i + 1] - sortedList[i] !== 1) {
+      isStraightFlag = false;
+    }
+  }
+
+  if (isStraightFlag) {
+    straight.position1 = sortedList[0];
+    straight.position2 = sortedList[1];
+    straight.position3 = sortedList[2];
+    straight.position4 = sortedList[3];
+    straight.position5 = sortedList[4];
+    straight.highest = sortedList[4];
+    straight.cards = list;
+    straight.rank = 5;
+  }
+
+  return straight;
+};
+
+const checkIfBetter = (oldStraight, newStraight) => {
+  return oldStraight.highest <= newStraight.highest;
+};
+
+const searchOnList = (straight, list) => {
+  let newStraight = searchForStraight(list);
+  if (newStraight.highest > 0) {
+    if (checkIfBetter(straight, newStraight)) {
+      straight = Object.assign({}, newStraight);
+      straight.rank = 5;
+    }
+  }
+  return straight;
+};
+
+const combinationsSearch = (
+  straight,
+  userCards,
+  replaceFromDeck,
+  replaceFromDeckLength
+) => {
+  let combinationsArray = combineGenerator(
+    userCards,
+    5 - replaceFromDeckLength
+  );
+  // builc array size 5 with cards from the replacmentDeck and the additional cards from user
+  for (let i = 0; i < combinationsArray.length; i++) {
+    let additionalCards = combinationsArray[i];
+
+    let searchList =
+      additionalCards === undefined
+        ? replaceFromDeck
+        : replaceFromDeck.concat(combinationsArray[i]);
+
+    if (searchList.length === 5) {
+      let newStraight = searchForStraight(searchList);
+      if (newStraight.value > 0) {
+        if (checkIfBetter(straight, newStraight)) {
+          straight = Object.assign({}, newStraight);
+          straight.rank = 5;
+        }
+      }
+    }
+  }
+
+  return straight;
+};
+export { isStraight };
